0f1f65ae42f527d156ed3f2d3aaf2a8ee643538b
Jesse Cranney <jesse.cranney@anu.edu.au>
2025-06-27 05:24:17 +1000

    using least_squares is much quicker and converges beautifully

--- validation.py
+++ validation.py
@@ -22,7 +22,7 @@ def build_imat(dms):
             pitch=dm["pitch"],
             nsubx=10,
             ntssamples=0,
-            nphisamples=21,
+            nphisamples=41,
             wfs_dirs=lgs_wfss,
             ts_dirs=[],
             dm_delta=dm["pert"],
@@ -35,22 +35,30 @@ def build_imat(dms):
     return imat
 
 
-dms_true = [
-    {"alt": 0.0, "pitch": 0.22, "pert": (0.00174467, 0.07772004)},
-    {"alt": 6000.0, "pitch": 0.25, "pert": (0.02740716, -0.2432777)},
-    {"alt": 13500.0, "pitch": 0.30, "pert": (0.09846721, -0.07259582)},
-]
-imat_true = build_imat(dms_true)
+def initialise_imat():
+    pert = [
+        (0.00174467, 0.07772004),
+        (0.02740716, -0.2432777),
+        (0.09846721, -0.07259582),
+    ]
+    dms = [
+        {"alt": 0.0, "pitch": 0.22, "pert": pert[0]},
+        {"alt": 6000.0, "pitch": 0.25, "pert": pert[1]},
+        {"alt": 13500.0, "pitch": 0.30, "pert": pert[2]},
+    ]
+    imat = build_imat(dms)
+    return imat, np.array(pert).flatten()
 
 
 def test_time():
     t1 = time.time()
-    build_imat(dms_true)
+    initialise_imat()
     t2 = time.time()
-    return t2-t1
+    return t2 - t1
 
 
 print(test_time())
+imat_true, pert_true = initialise_imat()
 
 
 def cost(pert):
@@ -59,10 +67,15 @@ def cost(pert):
         {"alt": 6000.0, "pitch": 0.25, "pert": (pert[2], pert[3])},
         {"alt": 13500.0, "pitch": 0.30, "pert": (pert[4], pert[5])},
     ]
-    err = np.linalg.norm(imat_true - build_imat(dms), ord="fro")
+    err = (imat_true - build_imat(dms)).flatten()
     return err
 
 
-pert_estimate = np.zeros(6)
-result = opt.minimize(cost, pert_estimate)
+t1 = time.time()
+pert_initial = np.zeros(6)
+result = opt.least_squares(cost, pert_initial)
+t2 = time.time()
+print(f"took {t2-t1:0.2f} seconds")
+pert_estimate = result["x"]
 print(result)
+print(pert_estimate, pert_true)
